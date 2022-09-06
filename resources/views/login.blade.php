df85d64e39fe529ed98ebda0b457bd431612307e
andrea-prina <and.prina@gmail.com>
2022-09-06 16:57:49 +0200

    Route tests

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel First Step</title>
+</head>
+<body>
+    
+    <input type="text" name="" id="" placeholder="username">
+    <input type="password" name="" id="" placeholder="password">
+    <button>LOGIN</button>
+
+</body>
+</html>
